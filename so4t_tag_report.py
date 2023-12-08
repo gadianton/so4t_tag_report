2106fc49c0f881e4e5d5c970020fae4b78377a1f
JK <61711289+jklick-so@users.noreply.github.com>
2023-12-08 07:03:59 -0500

    added response time metric

--- so4t_tag_report.py
+++ so4t_tag_report.py
@@ -11,7 +11,7 @@ import json
 import os
 import pickle
 import time
-from statistics import median
+import statistics
 
 # Local libraries
 from so4t_scraper import WebScraper
@@ -210,22 +210,26 @@ def get_tags(v3client):
     # making it a bit slower to get through. 
     # FUTURE WORK: implementing some form of concurrency would speed this up.
     for tag in tags:
-        tag['smes'] = v3client.get_tag_smes(tag['id']) 
+        if tag['subjectMatterExpertCount'] > 0:
+            tag['smes'] = v3client.get_tag_smes(tag['id']) 
+        else:
+            tag['smes'] = {'users': [], 'userGroups': []}
 
     return tags
 
 
-# def get_users(v3client):
+def get_users(v3client):
+    ### THIS FUNCTION IS NOT CURRENTLY USED ###
 
-#     users = v3client.get_all_users()
+    users = v3client.get_all_users()
 
-#     # Exclude users with an ID of less than 1 (i.e. Community user and user groups)
-#     users = [user for user in users if user['id'] > 1]
+    # Exclude users with an ID of less than 1 (i.e. Community user and user groups)
+    users = [user for user in users if user['id'] > 1]
 
-#     if 'soedemo' in v3client.api_url: # for internal testing only
-#         users = [user for user in users if user['id'] > 28000]
+    if 'soedemo' in v3client.api_url: # for internal testing only
+        users = [user for user in users if user['id'] > 28000]
 
-#     return users
+    return users
 
 
 def filter_api_data_by_date(api_data, days):
@@ -274,21 +278,43 @@ def process_api_data(api_data):
 
     # tally up miscellaneous metrics for each tag
     for tag in tags:
+        # Calculate unique contributors
         tag['metrics']['unique_askers'] = len(tag['contributors']['askers'])
         tag['metrics']['unique_answerers'] = len(tag['contributors']['answerers'])
         tag['metrics']['unique_commenters'] = len(tag['contributors']['commenters'])
         tag['metrics']['unique_article_contributors'] = len(
             tag['contributors']['article_contributors'])
-        tag['metrics']['unique_contributors'] = len(set(
+        tag['metrics']['total_unique_contributors'] = len(set(
             tag['contributors']['askers'] + 
             tag['contributors']['answerers'] +
             tag['contributors']['commenters'] + 
             tag['contributors']['article_contributors']))
         
+        # Calculate total self-answered questions
+        tag['metrics']['questions_self_answered'] = len(tag['self_answered_questions'])
+        
+        # Calculate median time to first answer and median time to first response
         try:
-            tag['metrics']['median_answer_time_hours'] = round(median(tag['answer_times']),2)
-        except ValueError: # if there are no answers for a tag
+            tag['metrics']['median_time_to_first_response_hours'] = round(statistics.median(
+                    [list(response.values())[0] for response in tag['response_times']]), 2)
+        except statistics.StatisticsError: # if there are no responses for a tag
             pass
+        
+        try:
+            tag['metrics']['median_time_to_first_answer_hours'] = round(statistics.median(
+                [list(answer.values())[0] for answer in tag['answer_times']]), 2)
+        except statistics.StatisticsError: # if there are no answers for a tag
+            pass
+
+        # Sort responses and answers by time to first response/answer, in descending order
+        tag['response_times'] = sorted(
+            tag['response_times'], 
+            key=lambda k: list(k.values())[0], 
+            reverse=True)
+        tag['answer_times'] = sorted(
+            tag['answer_times'], 
+            key=lambda k: list(k.values())[0], 
+            reverse=True)
     
     return tags
 
@@ -305,10 +331,12 @@ def process_tags(tags):
             'individual_smes': 0,
             'group_smes': 0,
             'total_unique_smes': 0,
+            'median_time_to_first_answer_hours': 0,
+            'median_time_to_first_response_hours': 0,
+            'total_unique_contributors': 0,
             'unique_askers': 0,
             'unique_answerers': 0,
             'unique_commenters': 0,
-            'unique_contributors': 0,
             'unique_article_contributors': 0,
             'question_count': 0,
             'question_upvotes': 0,
@@ -322,7 +350,6 @@ def process_tags(tags):
             'answer_upvotes': 0,
             'answer_downvotes': 0,
             'answer_comments': 0,
-            'median_answer_time_hours': 0,
             'article_count': 0,
             'article_upvotes': 0,
             'article_comments': 0,
@@ -335,8 +362,9 @@ def process_tags(tags):
             'individual_smes': [],
             'group_smes': []
         }
-        tag['users'] = {}
         tag['answer_times'] = []
+        tag['response_times'] = []
+        tag['self_answered_questions'] = []
 
         # calculate total unique SMEs, including individuals and groups
         for user in tag['smes']['users']:
@@ -362,35 +390,49 @@ def process_questions(tags, questions):
         for tag in question['tags']:
             tag_index = get_tag_index(tags, tag)
             tag_data = tags[tag_index]
-            tag_data, asker_id = validate_tag_user(tag_data, question['owner'])
+            asker_id = validate_user_id(question['owner'])
             
             tag_data['contributors']['askers'] = add_user_to_list(
                 asker_id, tag_data['contributors']['askers'])
-
-            tag_data['users'][asker_id]['question_upvotes'] += question['up_vote_count']
-            tag_data['users'][asker_id]['questions'] += 1
 
             tag_data['metrics']['question_count'] += 1
             tag_data['metrics']['total_page_views'] += question['view_count']
             tag_data['metrics']['question_upvotes'] += question['up_vote_count']
             tag_data['metrics']['question_downvotes'] += question['down_vote_count']
 
+            # Calculate tag metrics for comments
             if question.get('comments'):
-                tag_data['metrics']['question_comments'] += len(question['comments'])
-                for comment in question['comments']:
-                    tag_data, commenter_id = validate_tag_user(tag_data, comment['owner'])
-                    tag_data['contributors']['commenters'] = add_user_to_list(
-                        commenter_id, tag_data['contributors']['commenters'])
-                    
-                    tag_data['users'][commenter_id]['comments'] += 1
-                    tag_data['users'][commenter_id]['comment_upvotes'] += comment['score']
+                tag_data, time_to_first_comment = process_question_comments(
+                    tag_data, question)
+            else:
+                time_to_first_comment = 0
             
             # calculate tag metrics for answers
             if question.get('answers'):
-                tag_data = process_answers(tag_data, question['answers'], question)
+                tag_data, time_to_first_answer = process_answers(
+                    tag_data, question['answers'], question)
             else:
                 tag_data['metrics']['questions_no_answers'] += 1
+                time_to_first_answer = 0
 
+            # Calculate time to first response, which is the lesser of the time to first comment
+            # and the time to first answer
+            if time_to_first_answer > 0 and time_to_first_comment > 0:
+                time_to_first_response = min(time_to_first_answer, time_to_first_comment)
+            elif time_to_first_answer > 0:
+                time_to_first_response = time_to_first_answer
+            elif time_to_first_comment > 0:
+                # If the question is self-answered, the first comment is not considered a response
+                if question['link'] not in tag_data['self_answered_questions']:
+                    time_to_first_response = time_to_first_comment
+                else:
+                    time_to_first_response = None
+            else:
+                time_to_first_response = None
+
+            if time_to_first_response: # if there are no responses, don't add to list
+                tag_data['response_times'].append({question['link']: time_to_first_response})
+                                        
             tags[tag_index] = tag_data
 
     return tags
@@ -398,19 +440,14 @@ def process_questions(tags, questions):
         
 def process_answers(tag_data, answers, question):
 
-    answer_times = []
     for answer in answers:
-        tag_data, answerer_id = validate_tag_user(
-            tag_data, answer['owner'])
+        answerer_id = validate_user_id(answer['owner'])
         tag_data['contributors']['answerers'] = add_user_to_list(
             answerer_id, tag_data['contributors']['answerers'])
         if answer['is_accepted']:
             tag_data['metrics']['questions_accepted_answer'] += 1
-            tag_data['users'][answerer_id]['answers_accepted'] += 1
         tag_data['metrics']['answer_count'] += 1
-        tag_data['users'][answerer_id]['answers'] += 1
         tag_data['metrics']['answer_upvotes'] += answer['up_vote_count']
-        tag_data['users'][answerer_id]['answer_upvotes'] += answer['up_vote_count']
         tag_data['metrics']['answer_downvotes'] += answer['down_vote_count']
 
         # Calculate number of answers from SMEs
@@ -421,25 +458,70 @@ def process_answers(tag_data, answers, question):
         if answer.get('comments'):
             tag_data['metrics']['answer_comments'] += len(answer['comments'])
             for comment in answer['comments']:
-                tag_data, commenter_id = validate_tag_user(
-                    tag_data, comment['owner']
-                )
+                commenter_id = validate_user_id(comment['owner'])
                 tag_data['contributors']['commenters'] = add_user_to_list(
                     commenter_id, tag_data['contributors']['commenters']
                 )
-                tag_data['users'][commenter_id]['comments'] += 1
-                tag_data['users'][commenter_id]['comment_upvotes'] += comment['score']
 
-        answer_times.append(answer['creation_date'] - question['creation_date'])
+    # Calculate time to first answer (i.e. response) for questions
+    # Deleted answers do not show up in the API response; they are not included in the calculation
+        # This creates an outlier/edge case where the original answer was deleted and the next
+        # fastest answer is used instead, which could've been posted at a much later time
+    # If the fastest response is from the question asker, then the question is self-answered
+    # If the fastest answer owner does not have a `user_id` attribute, the owner was deleted
+    # If the owner of the question has a 'user_id', we can validate it was not self-answered
+    # If both uers have been deleted, the `display_name` attribute can be compared to see if they
+        # are the same person
+    time_to_first_answer = 0
+    if answers[0]['owner'].get('user_id'): # answer owner is known
+        if answers[0]['owner']['user_id'] != question['owner'].get('user_id'):
+            time_to_first_answer = (answers[0]['creation_date'] - question['creation_date'])/60/60
+        else: # if answer owner is the same as question owner, it's a self-answer
+            tag_data['self_answered_questions'].append(question['link'])
+    elif question['owner'].get('user_id'): # answer owner is unknown, but question owner is known
+        time_to_first_answer = (answers[0]['creation_date'] - question['creation_date'])/60/60
+    else: # if both question and answer owner are unknown, check display names for a match
+        if answers[0]['owner']['display_name'] == question['owner']['display_name']:
+            tag_data['self_answered_questions'].append(question['link'])
+        else:
+            time_to_first_answer = (answers[0]['creation_date'] - question['creation_date'])/60/60
 
-    # Use the fastest answer time as the answer time for the question
-    if min(answer_times) > 0:
-        answer_time_in_hours = min(answer_times)/60/60
-        tag_data['answer_times'].append(answer_time_in_hours)
-    else: # zero answer time means the answer was posted at same time as question
-        tag_data['metrics']['questions_self_answered'] += 1
+    if time_to_first_answer:
+        tag_data['answer_times'].append({question['link']: time_to_first_answer})
 
-    return tag_data
+    return tag_data, time_to_first_answer
+
+
+def process_question_comments(tag_data, question):
+
+    tag_data['metrics']['question_comments'] += len(question['comments'])
+    for comment in question['comments']:
+        commenter_id = validate_user_id(comment['owner'])
+        tag_data['contributors']['commenters'] = add_user_to_list(
+            commenter_id, tag_data['contributors']['commenters'])
+
+    # Calculate time to first comment
+    # There's an edge case where the first comment is from the question asker,
+        # where we may want to consider looking at subsequent comments
+        # May need to add a check for this
+    # If the fastest response is from the question asker, then disregard it
+    # If the fastest answer owner does not have a `user_id` attribute, the owner was deleted
+    # If the owner of the question has a 'user_id', we can validate it was not self-answered
+    # If both uers have been deleted, the `display_name` attribute can be compared to see if they
+        # are the same person
+    if question['comments'][0]['owner'].get('user_id'):
+        if question['comments'][0]['owner']['user_id'] != question['owner'].get('user_id'):
+            time_to_first_comment = (question['comments'][0]['creation_date'] - 
+                                    question['creation_date'])/60/60
+        else:
+            time_to_first_comment = 0
+    elif question['owner'].get('user_id'):
+        time_to_first_comment = (question['comments'][0]['creation_date'] - 
+                                    question['creation_date'])/60/60
+    else:
+        time_to_first_comment = 0
+
+    return tag_data, time_to_first_comment
 
 
 def process_articles(tags, articles):
@@ -448,20 +530,20 @@ def process_articles(tags, articles):
         for tag in article['tags']:
             tag_index = get_tag_index(tags, tag)
             tag_data = tags[tag_index]
-            tag_data, article_author_id = validate_tag_user(tag_data, article['owner'])
             tag_data['metrics']['total_page_views'] += article['view_count']
             tag_data['metrics']['article_count'] += 1
-            tag_data['users'][article_author_id]['articles'] += 1
             tag_data['metrics']['article_upvotes'] += article['score']
-            tag_data['users'][article_author_id]['article_upvotes'] += article['score']
             tag_data['metrics']['article_comments'] += article['comment_count']
-            tag_data['contributors']['article_contributors'] = add_user_to_list(
-                article_author_id, tag_data['contributors']['article_contributors']
-            )
             tag_data['metrics']['unique_article_contributors'] = len(
                 tag_data['contributors']['article_contributors'])
 
-            # As of 2023.05.23, Article comments are currently innaccurate due to a bug in the API
+            # Add article author to list of contributors
+            article_author_id = validate_user_id(article['owner'])
+            tag_data['contributors']['article_contributors'] = add_user_to_list(
+                article_author_id, tag_data['contributors']['article_contributors']
+            )
+
+            # As of 2023.05.23, Article comments are slightly innaccurate due to a bug in the API
             # if article.get('comments'):
             #     for comment in article['comments']:
             #         commenter_id = validate_user_id(comment)
@@ -474,9 +556,10 @@ def process_articles(tags, articles):
     return tags
 
 
-# def process_users(tags, users):
+def process_users(tags, users):
+    ### THIS FUNCTION IS NOT CURRENTLY USED ###
 
-#     return tags
+    return tags
 
 
 def process_communities(tags, communities):
@@ -546,38 +629,36 @@ def add_user_to_list(user_id, user_list):
     return user_list
 
 
-def validate_tag_user(tag, user):
+def validate_user_id(user):
 
     try:
         user_id = user['user_id']
-    except KeyError:
-        user_id = 'unknown'
-        user['display_name'] = None
-        user['link'] = None
-    
-    if user_id not in tag['users']:
-        tag['users'][user_id] = {
-            'id': user_id,
-            'name': user['display_name'],
-            'profile_url': user['link'],
-            'questions': 0,
-            'question_upvotes': 0,
-            'answers': 0,
-            'answer_upvotes': 0,
-            'answers_accepted': 0,
-            'articles': 0,
-            'article_upvotes': 0,
-            'comments': 0,
-            'comment_upvotes': 0,
-            'sme_individual': False,
-            'sme_group': False
-        }
-        if user_id in tag['contributors']['individual_smes']:
-            tag['users'][user_id]['sme_individual'] = True
-        if user_id in tag['contributors']['group_smes']:
-            tag['users'][user_id]['sme_group'] = True
+    except KeyError: # if user_id is not present, the user was deleted
+        user_id = f"{user['display_name']} (DELETED)"
 
-    return tag, user_id
+    # if user_id not in tag['users']:
+    #     tag['users'][user_id] = {
+    #         'id': user_id,
+    #         'name': user['display_name'],
+    #         'profile_url': user['link'],
+    #         'questions': 0,
+    #         'question_upvotes': 0,
+    #         'answers': 0,
+    #         'answer_upvotes': 0,
+    #         'answers_accepted': 0,
+    #         'articles': 0,
+    #         'article_upvotes': 0,
+    #         'comments': 0,
+    #         'comment_upvotes': 0,
+    #         'sme_individual': False,
+    #         'sme_group': False
+    #     }
+    #     if user_id in tag['contributors']['individual_smes']:
+    #         tag['users'][user_id]['sme_individual'] = True
+    #     if user_id in tag['contributors']['group_smes']:
+    #         tag['users'][user_id]['sme_group'] = True
+
+    return user_id
 
 
 def export_to_csv(data_name, data):
